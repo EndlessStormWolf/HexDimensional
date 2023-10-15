b045a03b6525c6c96893a1f210a47f32e2192640
WolfBrotherPC <wolfbrother@goldshadow.org>
2023-10-15 03:20:51 -0500

    Updated example stuff

--- src/main/java/org/endlessstormwolf/hexdimensional/mixin/HexDimensionalMixin.java
+++ src/main/java/org/endlessstormwolf/hexdimensional/mixin/HexDimensionalMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package org.endlessstormwolf.hexdimensional.mixin;
 
-import com.example.example_mod.ExampleMod;
+import org.endlessstormwolf.hexdimensional.HexDimensional;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public class HexDimensionalMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		HexDimensional.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
